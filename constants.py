072e87a872a64b9a9a7152c6af53f6991c131b63
Abdulrahman Awad <abdulrahmansawad@gmail.com>
2024-06-12 22:46:01 -0400

    updated documentations

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
-STUDENT_ALLOCATION_START = 59
-STUDENT_ALLOCATION_END = 80 
-SUBMISSIONS_ZIP_PATH = r"C:\Users\sheri\Downloads\Assignment 03 Download Jun 12, 2024 913 PM.zip"
-SUBMISSIONS_CSV_PATH = r"C:\Users\sheri\Downloads\submissions.csv"
-ASSIGNMENT_NAME = 'A3'
-TA_MARKING_PATH = r"C:\Users\sheri\OneDrive - Carleton University\Teaching Assistant Carleton\\" + ASSIGNMENT_NAME
+STUDENT_ALLOCATION_START = 59 # The number in the student list of the first student marking
+STUDENT_ALLOCATION_END = 80  # The number at the end of the student list of the last student marking
+SUBMISSIONS_ZIP_PATH = r"C:\Users\sheri\Downloads\Assignment 04 Download Jun 12, 2024 1038 PM.zip" # path to the zip file
+SUBMISSIONS_CSV_PATH = r"C:\Users\sheri\Downloads\submissions.csv" # Path to submissions CSV file from the prof
+ASSIGNMENT_NAME = 'A4' # Assignment name for marking folder
+TA_MARKING_PATH = r"C:\Users\sheri\OneDrive - Carleton University\Teaching Assistant Carleton\\" + ASSIGNMENT_NAME # Path to the directory where you are marking
